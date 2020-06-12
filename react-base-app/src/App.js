eaf916cbf663df0011738da927891b678e34e6a9
kashifyousaf <66198716+kashifyousaf@users.noreply.github.com>
2020-06-13 00:35:58 +0500

    update main.yml

--- react-base-app/src/App.js
+++ react-base-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div >
    <h1>hello kashif</h1>
    <h1>hello kashif</h1>
     </div>
