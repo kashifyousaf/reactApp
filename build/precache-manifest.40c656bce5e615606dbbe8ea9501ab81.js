80fe40391c48544a981619eeade4d38c1c838bf2
kashifyousaf <66198716+kashifyousaf@users.noreply.github.com>
2020-06-13 00:41:47 +0500

    change build and package,json

---
+++ build/precache-manifest.40c656bce5e615606dbbe8ea9501ab81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c8347edc2a5e691b037aeebe26d613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4acd453a036ed77ce2a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da8792238028bd1be30b",
+    "url": "/static/js/2.251b5031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.251b5031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4acd453a036ed77ce2a",
+    "url": "/static/js/main.ac8393e0.chunk.js"
+  },
+  {
+    "revision": "f5c81d5837c304670226",
+    "url": "/static/js/runtime-main.67db746e.js"
+  }
+]);
